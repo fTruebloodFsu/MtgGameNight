614f3b04691ef55aa8c6f26176a693fc8d0a3eb4
fTruebloodFsu <39035131+fTruebloodFsu@users.noreply.github.com>
2020-03-08 22:58:44 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,16 +4,28 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import { Container, Row, Col, Button } from 'reactstrap';
 
+var PLAYERS = 0;
+
 const diceStyle = {
   borderRadius: 5,
   borderColor: 'black',
   backgroundColor: 'lightgreen',
   color: 'black',
-  padding: 5,
-  margin: 5,
+  padding: 10,
+  margin: 10,
 };
 
-function NumPlayers(target){
+function onHoverEnter(ele){
+  ele.target.style.background = 'grey';
+  ele.target.style.color = 'white';
+}
+
+function onHoverLeave(ele){
+  ele.target.style.background = 'lightgreen';
+  ele.target.style.color = 'black';
+}
+
+function Start(target){
   ReactDOM.render(<NumPlayersPage />, document.getElementById('root'));
 }
 
@@ -21,25 +33,73 @@ function goBackToHomePage(target){
   ReactDOM.render(<App />, document.getElementById('root'));
 }
 
+function lifeTotals(numPlayers){
+  PLAYERS = numPlayers;
+  ReactDOM.render(<LifeTotalPage />, document.getElementById('root'));
+}
+
+function LifeTotalPage(){
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="BlackLotusBG.jpg" className="App-logo" alt="Black Lotus" style={{borderRadius: 10}} />
+        <p>Select the starting life total.</p>
+        <Container>
+          <Row>
+            <Col xs = {3}></Col>
+            <Col xs = {6} style = {diceStyle}
+                          onMouseOver={onHoverEnter} 
+                          onMouseLeave={onHoverLeave}>20</Col>
+          </Row>
+          <Row>
+            <Col xs = {3}></Col>
+            <Col xs = {6} style = {diceStyle}
+                          onMouseOver={onHoverEnter} 
+                          onMouseLeave={onHoverLeave}>40</Col>
+          </Row>
+          <Col xs = {12}>
+              <Button color="danger" onClick={goBackToHomePage}>Start Over</Button>
+          </Col>
+        </Container>
+      </header>
+    </div>
+  );
+}
+
+
 function NumPlayersPage(){
   return (
     <div className="App">
       <header className="App-header">
+      <img src="BlackLotusBG.jpg" className="App-logo" alt="Black Lotus" style={{borderRadius: 10}} />
+        <p>Select the number of players.</p>
         <Container>
           <Row>
-            <Col xs = {3} style={diceStyle}>1</Col>
-            <Col xs = {3} style={diceStyle}>2</Col>
+            <Col xs = {3}></Col>
+            <Col xs = {3} style={diceStyle} 
+                          onMouseOver={onHoverEnter} 
+                          onMouseLeave={onHoverLeave} 
+                          onClick={() => lifeTotals(1)}>1</Col>
+            <Col xs = {3} style={diceStyle} 
+                          onMouseOver={onHoverEnter} 
+                          onMouseLeave={onHoverLeave} 
+                          onClick={() => lifeTotals(2)}>2</Col>
           </Row>
           <Row>
-            <Col xs = {3} style={diceStyle}>3</Col>
-            <Col xs = {3} style={diceStyle}>4</Col>
+            <Col xs = {3}></Col>
+            <Col xs = {3} style={diceStyle} 
+                          onMouseOver={onHoverEnter} 
+                          onMouseLeave={onHoverLeave} 
+                          onClick={() => lifeTotals(3)}>3</Col>
+            <Col xs = {3} style={diceStyle} 
+                          onMouseOver={onHoverEnter} 
+                          onMouseLeave={onHoverLeave} 
+                          onClick={() => lifeTotals(4)}>4</Col>
           </Row>
           <Row>
-          <Col xs = {2}></Col>
-            <Col xs = {2}>
-              <Button color="danger" onClick={goBackToHomePage}> Back To Home </Button>
+            <Col xs = {12}>
+              <Button color="danger" onClick={goBackToHomePage}>Start Over</Button>
             </Col>
-            <Col xs = {2}></Col>
           </Row>
         </Container>
       </header>
@@ -53,7 +113,7 @@ function App() {
       <header className="App-header">
         <img src="BlackLotusBG.jpg" className="App-logo" alt="Black Lotus" style={{borderRadius: 10}} />
         <p>Welcome to MTG Game Night</p>
-        <button type="submit" class="btn btn-primary" onClick={NumPlayers}>Start a game!</button>
+        <button type="submit" class="btn btn-primary" onClick={Start}>Start a game!</button>
       </header>
     </div>
   );
