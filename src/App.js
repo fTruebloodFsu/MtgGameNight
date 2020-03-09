b3798d8a06f5b6245a2269f268d7421123c6d4a6
fTruebloodFsu <39035131+fTruebloodFsu@users.noreply.github.com>
2020-03-08 18:06:56 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import { Container, Row, Col, Button } from 'reactstrap';
 
-function NumPlayers(){
+const diceStyle = {
+  borderRadius: 5,
+  borderColor: 'black',
+  backgroundColor: 'lightgreen',
+  color: 'black',
+  padding: 5,
+  margin: 5,
+};
 
-  alert("hello");
+function NumPlayers(target){
+  ReactDOM.render(<NumPlayersPage />, document.getElementById('root'));
+}
+
+function goBackToHomePage(target){
+  ReactDOM.render(<App />, document.getElementById('root'));
+}
+
+function NumPlayersPage(){
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Container>
+          <Row>
+            <Col xs = {3} style={diceStyle}>1</Col>
+            <Col xs = {3} style={diceStyle}>2</Col>
+          </Row>
+          <Row>
+            <Col xs = {3} style={diceStyle}>3</Col>
+            <Col xs = {3} style={diceStyle}>4</Col>
+          </Row>
+          <Row>
+          <Col xs = {2}></Col>
+            <Col xs = {2}>
+              <Button color="danger" onClick={goBackToHomePage}> Back To Home </Button>
+            </Col>
+            <Col xs = {2}></Col>
+          </Row>
+        </Container>
+      </header>
+    </div>
+  );
 }
 
 function App() {
