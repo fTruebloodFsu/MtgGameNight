e5cc029e041799c280967a302cd50bd2ea1ac5ef
fTruebloodFsu <39035131+fTruebloodFsu@users.noreply.github.com>
2020-03-23 16:36:50 -0700

    Add files via upload
    added useState to control life totals on player 2,3,4

--- src/App.js
+++ src/App.js
@@ -80,6 +80,11 @@ function startGame(life){
 
 function GameTime4Player(){
 
+  const [life1, setLife1] = useState(LIFETOTAL);
+  const [life2, setLife2] = useState(LIFETOTAL);
+  const [life3, setLife3] = useState(LIFETOTAL);
+  const [life4, setLife4] = useState(LIFETOTAL);
+
   const styleFor4PlayerContainer = {
     height: '100vh',
     width: '100vw',
@@ -237,54 +242,6 @@ function GameTime4Player(){
     ele.target.style.background = 'slateblue';
   }
 
-  function incLife1(){
-    let currLife = parseInt(document.getElementById("P1Life").innerHTML);
-    currLife = currLife + 1;
-    document.getElementById("P1Life").innerHTML = currLife;
-   }
-   
-  function decLife1(){
-     let currLife = parseInt(document.getElementById("P1Life").innerHTML);
-     currLife = currLife - 1;
-     document.getElementById("P1Life").innerHTML = currLife;
-    }
-
-  function incLife2(){
-    let currLife = parseInt(document.getElementById("P2Life").innerHTML);
-    currLife = currLife + 1;
-    document.getElementById("P2Life").innerHTML = currLife;
-    }
-     
-  function decLife2(){
-      let currLife = parseInt(document.getElementById("P2Life").innerHTML);
-      currLife = currLife - 1;
-      document.getElementById("P2Life").innerHTML = currLife;
-    }
-
-  function incLife3(){
-    let currLife = parseInt(document.getElementById("P3Life").innerHTML);
-    currLife = currLife + 1;
-    document.getElementById("P3Life").innerHTML = currLife;
-  }
-       
-  function decLife3(){
-    let currLife = parseInt(document.getElementById("P3Life").innerHTML);
-    currLife = currLife - 1;
-    document.getElementById("P3Life").innerHTML = currLife;
-  }
-
-  function incLife4(){
-    let currLife = parseInt(document.getElementById("P4Life").innerHTML);
-    currLife = currLife + 1;
-    document.getElementById("P4Life").innerHTML = currLife;
-  }
-       
-  function decLife4(){
-    let currLife = parseInt(document.getElementById("P4Life").innerHTML);
-    currLife = currLife - 1;
-    document.getElementById("P4Life").innerHTML = currLife;
-  }
-
   return(
     <div className="4PlayerContainer" id="4PlayerContainer" style={styleFor4PlayerContainer}>
       <UtilityBar />
@@ -292,21 +249,21 @@ function GameTime4Player(){
         <div className="P1container" id="P1container" style={styleForPlayerContainer}>
           <div className="P1top" id="P1top" style={styleForP1} onMouseEnter={onHoverEnter}
                                                                onMouseLeave={onHoverLeave1}
-                                                               onClick={decLife1}></div>
-          <div className="P1Life" id="P1Life" style={styleForP1Life}>{LIFETOTAL}</div>
+                                                               onClick={() => setLife1(life1-1)}></div>
+          <div className="P1Life" id="P1Life" style={styleForP1Life}>{life1}</div>
           <div className="P1bottom" id="P1bottom" style={styleForP1} onMouseEnter={onHoverEnter}
                                                                      onMouseLeave={onHoverLeave1}
-                                                                     onClick={incLife1}></div>
+                                                                     onClick={() => setLife1(life1+1)}></div>
         </div>
 
         <div className="P2container" id="P2container" style={styleForPlayerContainer}>
           <div className="P2top" id="P2top" style={styleForP2} onMouseEnter={onHoverEnter}
                                                                onMouseLeave={onHoverLeave2}
-                                                               onClick={incLife2}></div>
-          <div className="P2Life" id="P2Life" style={styleForP2Life}>{LIFETOTAL}</div>
+                                                               onClick={() => setLife2(life2+1)}></div>
+          <div className="P2Life" id="P2Life" style={styleForP2Life}>{life2}</div>
           <div className="P2bottom" id="P2bottom" style={styleForP2} onMouseEnter={onHoverEnter}
                                                                      onMouseLeave={onHoverLeave2}
-                                                                     onClick={decLife2}></div>
+                                                                     onClick={() => setLife2(life2-1)}></div>
         </div>
       </div>
 
@@ -314,21 +271,21 @@ function GameTime4Player(){
         <div className="P3container" id="P3container" style={styleForPlayerContainer}>
           <div className="P3top" id="P3top" style={styleForP3} onMouseEnter={onHoverEnter}
                                                                onMouseLeave={onHoverLeave3}
-                                                               onClick={decLife3}></div>
-          <div className="P3Life" id="P3Life" style={styleForP3Life}>{LIFETOTAL}</div>
+                                                               onClick={() => setLife3(life3-1)}></div>
+          <div className="P3Life" id="P3Life" style={styleForP3Life}>{life3}</div>
           <div className="P3bottom" id="P3bottom" style={styleForP3} onMouseEnter={onHoverEnter}
                                                                      onMouseLeave={onHoverLeave3}
-                                                                     onClick={incLife3}></div>
+                                                                     onClick={() => setLife3(life3+1)}></div>
         </div>
 
         <div className="P4container" id="P4container" style={styleForPlayerContainer}>
           <div className="P4top" id="P4top" style={styleForP4} onMouseEnter={onHoverEnter}
                                                                onMouseLeave={onHoverLeave4}
-                                                               onClick={incLife4}></div>
-          <div className="P4Life" id="P4Life" style={styleForP4Life}>{LIFETOTAL}</div>
+                                                               onClick={() => setLife4(life4+1)}></div>
+          <div className="P4Life" id="P4Life" style={styleForP4Life}>{life4}</div>
           <div className="P4bottom" id="P4bottom" style={styleForP4} onMouseEnter={onHoverEnter}
                                                                      onMouseLeave={onHoverLeave4}
-                                                                     onClick={decLife4}></div>
+                                                                     onClick={() => setLife4(life4-1)}></div>
         </div>
       </div>
     </div>
@@ -336,6 +293,10 @@ function GameTime4Player(){
 }
 
 function GameTime3Player(){
+
+  const [life1, setLife1] = useState(LIFETOTAL);
+  const [life2, setLife2] = useState(LIFETOTAL);
+  const [life3, setLife3] = useState(LIFETOTAL);
 
   const styleFor3PlayerContainer = {
     height: '100vh',
@@ -460,70 +421,34 @@ function GameTime3Player(){
     ele.target.style.background = 'yellow';
   }
 
-  function incLife1(){
-    let currLife = parseInt(document.getElementById("P1Life").innerHTML);
-    currLife = currLife + 1;
-    document.getElementById("P1Life").innerHTML = currLife;
-   }
-   
-  function decLife1(){
-     let currLife = parseInt(document.getElementById("P1Life").innerHTML);
-     currLife = currLife - 1;
-     document.getElementById("P1Life").innerHTML = currLife;
-    }
-
-    function incLife2(){
-      let currLife = parseInt(document.getElementById("P2Life").innerHTML);
-      currLife = currLife + 1;
-      document.getElementById("P2Life").innerHTML = currLife;
-     }
-     
-  function decLife2(){
-       let currLife = parseInt(document.getElementById("P2Life").innerHTML);
-       currLife = currLife - 1;
-       document.getElementById("P2Life").innerHTML = currLife;
-      }
-
-  function incLife3(){
-      let currLife = parseInt(document.getElementById("P3Life").innerHTML);
-      currLife = currLife + 1;
-      document.getElementById("P3Life").innerHTML = currLife;
-  }
-       
-  function decLife3(){
-      let currLife = parseInt(document.getElementById("P3Life").innerHTML);
-      currLife = currLife - 1;
-      document.getElementById("P3Life").innerHTML = currLife;
-  }
-
   return (
     <div className="3PlayerContainer" id="3PlayerContainer" style={styleFor3PlayerContainer}>
       <UtilityBar />
       <div className="player1Top" id="player1Top" style={styleForPlayer1} onMouseEnter={onHoverEnter}
                                                                           onMouseLeave={onHoverLeave1}
-                                                                          onClick={incLife1}></div>
-      <div className="P1Life" id="P1Life" style={lifeTotalStyle1}>{LIFETOTAL}</div>
+                                                                          onClick={() => setLife1(life1+1)}></div>
+      <div className="P1Life" id="P1Life" style={lifeTotalStyle1}>{life1}</div>
       <div className="player1bottom" id="player1bottom" style={styleForPlayer1}onMouseEnter={onHoverEnter}
                                                                           onMouseLeave={onHoverLeave1}
-                                                                          onClick={decLife1}></div>
+                                                                          onClick={() => setLife1(life1-1)}></div>
       <div className="bottom2players" id="bottom2players" style={bottom2Players}>
         <div className="player2container" id="player2container" style={bottomPlayerContainer}>
           <div className="player2top" id="player2top" style={styleForPlayer2} onMouseEnter={onHoverEnter}
                                                                               onMouseLeave={onHoverLeave2}
-                                                                              onClick={decLife2}></div>
-          <div className="P2Life" id="P2Life" style={lifeTotalStyle2}>{LIFETOTAL}</div>
+                                                                              onClick={() => setLife2(life2-1)}></div>
+          <div className="P2Life" id="P2Life" style={lifeTotalStyle2}>{life2}</div>
           <div className="player2bottom" id="player2bottom" style={styleForPlayer2} onMouseEnter={onHoverEnter}
                                                                                     onMouseLeave={onHoverLeave2}
-                                                                                    onClick={incLife2}></div>
+                                                                                    onClick={() => setLife2(life2+1)}></div>
         </div>
         <div className="player3container" id="plaer3container" style={bottomPlayerContainer}>
           <div className="player3top" id="player3top" style={styleForPlayer3} onMouseEnter={onHoverEnter}
                                                                               onMouseLeave={onHoverLeave3}
-                                                                              onClick={incLife3}></div>
-          <div className="P3Life" id="P3Life" style={lifeTotalStyle3}>{LIFETOTAL}</div>
+                                                                              onClick={() => setLife3(life3+1)}></div>
+          <div className="P3Life" id="P3Life" style={lifeTotalStyle3}>{life3}</div>
           <div className="player3bottom" id="player3bottom" style={styleForPlayer3} onMouseEnter={onHoverEnter}
                                                                                     onMouseLeave={onHoverLeave3}
-                                                                                    onClick={decLife3}></div>
+                                                                                    onClick={() => setLife3(life3-1)}></div>
         </div>
       </div>
     </div>
@@ -532,6 +457,9 @@ function GameTime3Player(){
 }
 
 function GameTime2Player(){
+
+  const [life1, setLife1] = useState(LIFETOTAL); 
+  const [life2, setLife2] = useState(LIFETOTAL); 
 
   const styleFor2PlayerContainer = {
     height: '100vh',
@@ -605,48 +533,24 @@ function GameTime2Player(){
     ele.target.style.background = 'mediumvioletred';
   }
 
-  function incLife1(){
-    let currLife = parseInt(document.getElementById("P1Life").innerHTML);
-    currLife = currLife + 1;
-    document.getElementById("P1Life").innerHTML = currLife;
-   }
-   
-  function decLife1(){
-     let currLife = parseInt(document.getElementById("P1Life").innerHTML);
-     currLife = currLife - 1;
-     document.getElementById("P1Life").innerHTML = currLife;
-    }
-
-  function incLife2(){
-      let currLife = parseInt(document.getElementById("P2Life").innerHTML);
-      currLife = currLife + 1;
-      document.getElementById("P2Life").innerHTML = currLife;
-     }
-     
-  function decLife2(){
-       let currLife = parseInt(document.getElementById("P2Life").innerHTML);
-       currLife = currLife - 1;
-       document.getElementById("P2Life").innerHTML = currLife;
-      }
-
   return (
     <div className="2PlayerContainer" id="2PlayerContainer" style={styleFor2PlayerContainer}>
       <UtilityBar />
       <div className="Player1top" id="Player1top" style={playerStyle1} onMouseOver={onHoverEnter}
                                                                        onMouseLeave={onHoverLeave1}
-                                                                       onClick={incLife1}></div>
-      <div ClassName="P1Life" id="P1Life" style={lifeTotalStyle1}>{LIFETOTAL}</div>
+                                                                       onClick={() => setLife1(life1+1)}></div>
+      <div ClassName="P1Life" id="P1Life" style={lifeTotalStyle1}>{life1}</div>
       <div className="Player1bottom" id="Player1bottom" style={playerStyle1} onMouseOver={onHoverEnter}
                                                                        onMouseLeave={onHoverLeave1}
-                                                                       onClick={decLife1}></div>
+                                                                       onClick={() => setLife1(life1-1)}></div>
 
       <div className="Player2top" id="Player2top" style={playerStyle2} onMouseOver={onHoverEnter}
                                                                        onMouseLeave={onHoverLeave2}
-                                                                       onClick={incLife2}></div>
-      <div ClassName="P2Life" id="P2Life" style={lifeTotalStyle2}>{LIFETOTAL}</div>
+                                                                       onClick={() => setLife2(life2+1)}></div>
+      <div ClassName="P2Life" id="P2Life" style={lifeTotalStyle2}>{life2}</div>
       <div className="Player2bottom" id="Player2bottom" style={playerStyle2} onMouseOver={onHoverEnter}
                                                                        onMouseLeave={onHoverLeave2}
-                                                                       onClick={decLife2}></div>                
+                                                                       onClick={() => setLife2(life2-1)}></div>                
     </div>
   );
 
@@ -655,7 +559,6 @@ function GameTime2Player(){
 function GameTime1Player(){
 
   const [life, setLife] = useState(LIFETOTAL);
-  // const [colorTop, setColorTop] = useState(false)
 
   function onHoverEnter(ele){
     ele.target.style.background = 'grey';
